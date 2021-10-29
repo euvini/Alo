652dade8b15fd8a3ab6dd85e5a25280a0b0b6e44
Vinicius Santiago <60615218+euvini@users.noreply.github.com>
2021-10-29 15:41:24 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { Link } from 'react-router-dom';
 
+import { FiFacebook, FiInstagram, FiTwitch, FiTwitter } from 'react-icons/fi';
+import logo from '../../assets/logo-white.svg';
+import './styles.css';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+export default function Plan(dados) {
+console.log(dados);
+    return (
+        <div className="plan-container">
+            <header>
+                <img className="logo-plan" src={logo} alt="logo-white" />
+                <span>Você está em {dados.location.state.bairro}, {dados.location.state.localidade} - {dados.location.state.uf}</span>
+            </header>
+            <div>
+                <h1>As melhores ofertas para você</h1>
+            </div>
+            <section className="cards">
+                <ul>
+                    <li>
+                        <strong>Plano Hey!</strong>
+                        <h3>20GB</h3>
+                        <span>Aplicativos sem consumir dos dados</span>
+                        <p>
+                            <FiFacebook size={22} color="#DD2A7B" />
+                            <FiInstagram size={22} color="#DD2A7B" />
+                        </p>
+                        <button type="button">ASSINAR</button>
+                    </li>
 
+                    <li>
+                        <strong>Plano Talk!</strong>
+                        <h3>40GB</h3>
+                        <span>Aplicativos sem consumir dos dados</span>
+                        <p>
+                            <FiFacebook size={22} color="#DD2A7B" />
+                            <FiInstagram size={22} color="#DD2A7B" />
+                            <FiTwitter size={22} color="#DD2A7B" />
+                        </p>
+                        <button type="button">ASSINAR</button>
+                    </li>
 
+                    <li>
+                        <strong>Plano Alô!</strong>
+                        <h3>60GB</h3>
+                        <span>Aplicativos sem consumir dos dados</span>
+                        <p>
+                            <FiFacebook size={22} color="#DD2A7B" />
+                            <FiInstagram size={22} color="#DD2A7B" />
+                            <FiTwitter size={22} color="#DD2A7B" />
+                            <FiTwitch size={22} color="#DD2A7B" />
+                        </p>
+                        <button type="button">ASSINAR</button>
+                    </li>
+                </ul>
+            </section>
+            <div className="footer">
+                <Link className="link-back" to="/">
+                    <button className="button-back" type="button">Ops, errei meu CEP</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
